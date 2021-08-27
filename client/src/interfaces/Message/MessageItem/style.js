fff6be45ad09e62c4339c711843c4118daa2b3f5
Vitor Livi <44207509+VitorLivi@users.noreply.github.com>
2021-08-26 23:44:26 -0300

    Fixing folder bug git

--- client/src/interfaces/Message/MessageItem/style.js
+++
@@ -1,9 +0,0 @@
-import styled from 'styled-components'
-import { TailOptions } from '../../../Components/Balloon'
-
-export const MessageItemLayout = styled.div`
-  display: flex;
-  gap: 30px;
-
-  flex-direction: ${({ tail }) => tail === TailOptions.TAIL_RIGHT ? 'row-reverse' : 'row'};
-`
